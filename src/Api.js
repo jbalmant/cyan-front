16f5b162d5b1d070a7c1a247b3d5c43c22df7e1a
Jonatas Balmant <jbalmant@live.com>
2020-12-01 06:42:59 -0300

    Heroku working

--- src/Api.js
+++ src/Api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // TODO (jbalmant) - Add auth
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/',
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/',
   responseType: "json",
   headers: {
       'Access-Control-Allow-Origin': '*'
